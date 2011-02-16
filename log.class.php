e218d195a78aabf3214bf1aa4ce52095f2fa9ef2
Patrik Šíma <ja@patriksima.cz>
2011-02-17 00:39:11 +0100

    log class

---
+++ log.class.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Author: Patrik Šíma <ja@patriksima.cz>
+ * License: MIT
+ */
+
+class Log
+{
+    protected static $table = '';
+    protected static $expire = 30; //days
+
+    protected static function Rec ( $level, $message, $params )
+    {
+        $sql = "INSERT INTO `".Log::$table."`
+                        SET level = '".$level."',
+                            message = '".mysql_escape_string($message)."',
+                            params = '".serialize($params)."',
+                            recorded = NOW()";
+        $res = mysql_query($sql);
+
+        Log::Rotate();
+    }
+    
+    public static function Info( $message, $params = '' )
+    {
+        Log::Rec('info', $message, $params);
+    }
+    
+    public static function Warn( $message, $params = '' )
+    {
+        Log::Rec('warn', $message, $params);
+    }
+    
+    public static function Error( $message, $params = '' )
+    {
+        Log::Rec('error', $message, $params);
+    }
+
+    public static function Rotate()
+    {
+        $sql = "DELETE FROM `".Log::$table."` WHERE DATE_ADD(recorded, INTERVAL ".Log::$expire." DAY) < NOW()";
+        $res = mysql_query($sql);
+    }
+}
+?>
