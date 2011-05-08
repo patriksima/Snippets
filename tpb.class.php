1b1131c6bb344bb380e5797052bdd46f64274485
Patrik Šíma <ja@patriksima.cz>
2011-05-08 14:04:42 +0200

    release type

--- tpb.class.php
+++ tpb.class.php
@@ -19,7 +19,7 @@ class TPB
 
         if (preg_match_all('/<td>\W*<div class="detName">(?:(?!<\/td>).)*<\/td>/msU', $html, $matches)) {
             foreach($matches[0] as $item) {
-                $torrent = array('name'=>'', 'magnet'=>'', 'size'=>'');
+                $torrent = array('name'=>'', 'magnet'=>'', 'size'=>'', 'type'=>'');
                 if (preg_match('/<a[^>]*class="detLink"[^>]*>((?:(?!<\/a>).)*)<\/a>/msU', $item, $match)) {
                     $torrent['name'] = trim(html_entity_decode(strip_tags($match[1]), ENT_QUOTES, 'UTF-8'));
                 }
@@ -28,6 +28,9 @@ class TPB
                 }
                 if (preg_match('/<font class="detDesc">.*Size ([^,]+)/ms', $item, $match)) {
                     $torrent['size'] = trim(html_entity_decode(strip_tags($match[1]), ENT_QUOTES, 'UTF-8'));
+                }
+                if (preg_match('/(dvdrip|bdrip|brrip|dvdscr|ppvrip)/msi', $torrent['name'], $match)) {
+                    $torrent['type'] = trim(html_entity_decode(strip_tags($match[1]), ENT_QUOTES, 'UTF-8'));
                 }
                 $list[] = $torrent;
             }
